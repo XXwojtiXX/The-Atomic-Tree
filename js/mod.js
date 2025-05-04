82d4a7aebfd0fbcef3c64c241ea6bddafc3deb8d
XXwojtiXX <xxwojtixx@interia.pl>
2025-05-04 20:42:03 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,8 +42,8 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	return gain
 	if (hasUpgrade('h', 11)) gain = gain.times(2)
+	return gain
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
